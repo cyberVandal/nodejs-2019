4e1c704502db43fe179fe3aca10971866f9f0e65
Gjorgji Argirovski <argirovski.g@gmail.com>
2019-04-03 22:03:46 +0200

    Chas 16

---
+++ c1/v1.js
@@ -0,0 +1,21 @@
+console.log('TEST');
+
+var ime = "Goran";
+var godini = 31;
+var visina = 1.82;
+var vikend = false;
+
+
+var condition = true && (true || false) || false;
+
+
+if(condition){
+
+    console.log("TRUE");
+}else{
+
+    console.log("FALSE");
+
+}
+
+   
