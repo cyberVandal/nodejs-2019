fbd81134897fee63e1d84ef1c323edf265b73054
Goran Mitkovski <goran_mitkovski@yahoo.com>
2019-04-08 22:04:40 +0200

    treti cas

---
+++ c3/data/students.js
@@ -0,0 +1,14 @@
+var students = [
+    {firs_name: 'Pero', last_name: 'Perovski', gpa: 7.3},
+    {firs_name: 'Slave', last_name: 'Slavkovski', gpa: 7.5},
+    {firs_name: 'Trajko', last_name: 'Trajkovski', gpa: 8.3},
+    {firs_name: 'Kiro', last_name: 'Kirovski', gpa: 6.3}
+];
+
+
+module.exports = {
+
+
+    students
+
+}
---
+++ c3/handlers/students.js
@@ -0,0 +1,52 @@
+var data = require('../data/students');
+
+var getAll = (req, res) => {
+    res.send(data.students);
+}
+
+var getOne = (req, res) => {
+    var id = req.param('id');
+    if(data.students[id] != undefined){
+        return res.send(data.students[id]);
+    }
+    return res.status(404).send('Not Faund');
+}
+
+var add = (req, res) => {
+
+    var validate = req.body.firs_name !=undefined
+        && req.body.last_name !=undefined
+        && req.body.gpa !=undefined
+
+        if(validate){
+            data.students.push({
+                firs_name: req.body.firs_name,
+                last_name: req.body.last_name,
+                gpa: req.body.gpa
+            });
+            return res.status(201).send(req.body);
+        }
+    return res.status(400).send('Bad request');
+}
+
+var remove = (req, res) => {
+    res.send('OK');
+}
+
+var update = (req, res) => {
+    res.send('OK');
+}
+
+var patch = (req, res) => {
+    res.send('OK');
+}
+
+module.exports = {
+
+getAll,
+getOne,
+add,
+remove,
+update,
+patch
+}
